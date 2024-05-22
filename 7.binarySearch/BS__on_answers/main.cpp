5cb64bbbdf6a2b4e498f938601b26d643aa913e5
= <harmostparmod@gmail.com>
2024-05-22 08:21:34 +0530

    7.binarySearch [ on answers completed]

--- 7.binarySearch/BS__on_answers/main.cpp
+++ 7.binarySearch/BS__on_answers/main.cpp
@@ -194,6 +194,200 @@ int shipCapacity(vector<int> arr, int days)
     return ans;
 }
 
+int kMissing(int arr[], int n, int k)
+{
+    int l = 0;
+    int h = n - 1;
+    while (l <= h)
+    {
+        cout << " l : " << l << endl;
+        int mid = l + (h - l) / 2;
+        int missing = arr[mid] - (mid + 1);
+        if (missing < k)
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+    cout << " l2 : " << l << endl;
+    cout << k << endl;
+    return l + k;
+}
+
+bool canPlace(int arr[], int n, int mid, int cows)
+{
+    int count = 1;
+    int last = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] - last >= mid)
+        {
+            count++;
+            last = arr[i];
+        }
+        if (count >= cows)
+            return true;
+    }
+    return false;
+}
+
+int aggCows(int arr[], int n, int cows)
+{
+    sort(arr + 0, arr + n);
+    int l = 0;
+    int h = (arr[n - 1] - arr[0]);
+    while (l <= h)
+    {
+        int mid = l + (h - l) / 2;
+        if (canPlace(arr, n, mid, cows))
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+    return h;
+}
+
+int countStudents(vector<int> &arr, int pages)
+{
+    int n = arr.size();
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pagesStudent + arr[i] <= pages)
+        {
+            pagesStudent += arr[i];
+        }
+        else
+        {
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int> &arr, int n, int m)
+{
+    if (m > n)
+        return -1;
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+double median(vector<int> &a, vector<int> &b)
+{
+    int n1 = a.size(), n2 = b.size();
+    if (n1 > n2)
+        return median(b, a);
+
+    int n = n1 + n2;
+    int left = (n1 + n2 + 1) / 2;
+    int low = 0, high = n1;
+    while (low <= high)
+    {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1)
+            r1 = a[mid1];
+        if (mid2 < n2)
+            r2 = b[mid2];
+        if (mid1 - 1 >= 0)
+            l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0)
+            l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1)
+        {
+            if (n % 2 == 1)
+                return max(l1, l2);
+            else
+                return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        }
+
+        else if (l1 > r2)
+            high = mid1 - 1;
+        else
+            low = mid1 + 1;
+    }
+    return 0;
+}
+
+int kTwoSorted(int arr[], int n, int brr[], int m, int k)
+{
+    int index = 0;
+    int ans = -1;
+    int i = 0;
+    int j = 0;
+    while (i < n && j < m)
+    {
+
+        if (index == k)
+        {
+            return ans;
+        }
+
+        if (arr[i] <= brr[j])
+        {
+            ans = arr[i];
+            index = index + 1;
+            i++;
+        }
+        else
+        {
+            ans = brr[j];
+            index = index + 1;
+            j++;
+        }
+    }
+
+    // hndling the i j position case
+    if (i == n)
+        i += 1;
+    else if (j == n)
+        j += 1;
+
+    while (i <= n)
+    {
+        if (index == k)
+        {
+            return ans;
+        }
+        ans = arr[i];
+        index = index + 1;
+        i++;
+    }
+
+    while (j <= m)
+    {
+        if (index == k)
+        {
+            return ans;
+        }
+        ans = brr[j];
+
+        index = index + 1;
+        j++;
+    }
+    return -1;
+}
+
 int main()
 {
 
@@ -241,6 +435,47 @@ int main()
     // int n = 10;
     // int day = 5;
     // int ans = shipCapacity(arr, n, day);
+    // cout << "ans : " << ans << endl;
+
+    // ============================================================================
+    //  Kth missing positive number in an array
+    // int arr[] = {2, 3, 4, 7, 11};
+    // int n = 5;
+    // int k = 5;
+    // int ans = kMissing(arr, n, k);
+    // cout << "ans : " << ans << endl;
+
+    // ============================================================================
+    //  Aggressive cows
+    // int arr[] = {5, 4, 3, 2, 1, 1000000000};
+    // int n = 6;
+    // int cows = 2;
+    // int ans = aggCows(arr, n, cows);
+    // cout << "ans : " << ans << endl;
+
+    // ============================================================================
+    //  Book allocation
+    // vector<int> arr = {25, 46, 28, 49, 24};
+    // int n = 5;
+    // int m = 4;
+    // int ans = findPages(arr, n, m);
+    // cout << ans;
+
+    // ============================================================================
+    //  median of 2 sorted arrays
+    // vector<int> arr = {1, 2, 3, 4};
+    // vector<int> brr = {3, 4, 5};
+    // double ans = median(arr, brr);
+    // cout << "ans : " << ans << endl;
+
+    // ============================================================================
+    //  k-th element of 2 sorted arrays
+    // int arr[] = {2, 3, 6, 12, 15, 18};
+    // int brr[] = {1, 4, 8, 10};
+    // int n = 6;
+    // int m = 4;
+    // int k = 8;
+    // int ans = kTwoSorted(arr, n, brr, m, k);
     // cout << "ans : " << ans << endl;
 
     return 0;
