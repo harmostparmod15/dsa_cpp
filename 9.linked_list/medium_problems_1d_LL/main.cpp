35eb9b7fee1eaf5059e7de4f74f32fd7c6597039
parmod sharma <harmostparmod@gmail.com>
2024-06-06 08:02:38 +0530

    9.linked_list [medium problems completed]

--- 9.linked_list/medium_problems_1d_LL/main.cpp
+++ 9.linked_list/medium_problems_1d_LL/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <map>
 using namespace std;
 
@@ -42,9 +43,10 @@ void print(Node *head)
     Node *temp = head;
     while (temp)
     {
-        cout << temp->data;
+        cout << temp->data << "   ";
         temp = temp->next;
     }
+    cout << endl;
 }
 
 Node *findMiddleNode(Node *head)
@@ -306,18 +308,402 @@ Node *removeNthNodeBack(Node *head, int n)
     return head;
 }
 
+Node *removeMiddle(Node *head)
+{
+    // optimal T.C = o(n/2)|| S.C = o(1)
+    Node *slow = head;
+    Node *fast = head;
+    Node *beforeSlow = slow;
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    while (fast != NULL && fast->next != NULL)
+    {
+        beforeSlow = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    beforeSlow->next = beforeSlow->next->next;
+    delete slow;
+    return head;
+}
+
+Node *findMiddleVersion2(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head->next;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+Node *merge2List(Node *list1, Node *list2)
+{
+    Node *dummyNode = new Node(-1);
+    Node *temp = dummyNode;
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->data < list2->data)
+        {
+            temp->next = list1;
+            temp = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            temp->next = list2;
+            temp = list2;
+            list2 = list2->next;
+        }
+    }
+    if (list1)
+        temp->next = list1;
+    else
+        temp->next = list2;
+    return dummyNode->next;
+}
+
+Node *sortLL(Node *head)
+{
+    // brute  T.C = ( o(n) + o(logn) + o(n) )  S.C = o(n)
+    // 1 : take out from ll and put in vector and sort vector
+    // vector<int> v;
+    // Node *temp = head;
+    // while (temp != NULL)
+    // {
+    //     v.push_back(temp->data);
+    //     temp = temp->next;
+    // }
+    // sort(v.begin(), v.end());
+    // // 2 : put back in ll
+    // temp = head;
+    // int i = 0;
+    // while (temp != NULL)
+    // {
+    //     temp->data = v[i];
+    //     i++;
+    //     temp = temp->next;
+    // }
+    // return head;
+
+    // optimal
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    Node *middle = findMiddleVersion2(head);
+    Node *leftHead = head;
+    Node *rightHead = middle->next;
+    middle->next = NULL;
+
+    leftHead = sortLL(leftHead);
+    rightHead = sortLL(rightHead);
+
+    return merge2List(leftHead, rightHead);
+}
+
+Node *sortZeroOneTwo(Node *head)
+{
+    // brute T.C = o(2n) || S.C = o(1)
+    // Node *temp = head;
+    // int cnt0 = 0;
+    // int cnt1 = 0;
+    // int cnt2 = 0;
+    // while (temp != NULL)
+    // {
+    //     if (temp->data == 0)
+    //         cnt0++;
+    //     else if (temp->data == 1)
+    //         cnt1++;
+    //     else
+    //         cnt2++;
+    //     temp = temp->next;
+    // }
+    // temp = head;
+    // while (temp != NULL)
+    // {
+    //     if (cnt0 > 0)
+    //     {
+    //         temp->data = 0;
+    //         cnt0--;
+    //     }
+    //     else if (cnt1 > 0)
+    //     {
+    //         temp->data = 1;
+    //         cnt1--;
+    //     }
+    //     else
+    //     {
+    //         temp->data = 2;
+    //         cnt2--;
+    //     }
+    //     temp = temp->next;
+    // }
+    // return head;
+
+    // optimal T.C =o(n) || S.C = o(1)
+    if (head == NULL || head->next == NULL)
+        return head;
+    // 1 : create 3 dummy LL and pointers to traverse in list
+    Node *zeroHead = new Node(-1);
+    Node *oneHead = new Node(-1);
+    Node *twoHead = new Node(-1);
+    Node *zero = zeroHead;
+    Node *one = oneHead;
+    Node *two = twoHead;
+    Node *temp = head;
+
+    // 2 : inserting in the 3 created LL the values with pointers
+    while (temp != NULL)
+    {
+        if (temp->data == 0)
+        {
+            zero->next = temp;
+            zero = temp;
+        }
+        else if (temp->data == 1)
+        {
+            one->next = temp;
+            one = temp;
+        }
+        else
+        {
+            two->next = temp;
+            two = temp;
+        }
+        temp = temp->next;
+    }
+    // 3 : connecting the 3 pointers with one another
+    zero->next = (oneHead->next) ? oneHead->next : twoHead->next;
+    one->next = twoHead->next;
+    two->next = NULL;
+    Node *newHead = zeroHead->next;
+    // 4 : free up space
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    return newHead;
+}
+
+bool isPalindrome(Node *head)
+{
+    // brute T.C = o(2n) || S.C = o(n)
+    // stack<Node *> st;
+    // Node *temp = head;
+    // // 1 : insert nodes in stack
+    // while (temp)
+    // {
+    //     st.push(temp);
+    //     temp = temp->next;
+    // }
+    // // 2 : compare top of stack node and current node of LL
+    // temp = head;
+    // bool flag = false;
+    // while (temp != NULL)
+    // {
+    //     if (st.top()->data == temp->data)
+    //     {
+    //         flag = true;
+    //         st.pop();
+    //         temp = temp->next;
+    //     }
+    //     else
+    //     {
+    //         flag = false;
+    //         break;
+    //     }
+    // }
+    // return flag;
+
+    // optimal T.C = o(2n) || S.C = o(1)
+    Node *slow = head;
+    Node *fast = head;
+    // 1: find the middle node
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    // 2 :  reverse the LL after middle node
+    Node *newHead = reverseLL(slow->next);
+
+    // 3 : start comparing from start of LL and after middle of LL
+    Node *first = head;
+    Node *second = newHead;
+    while (second != NULL)
+    {
+        if (first->data != second->data)
+        {
+            head = reverseLL(newHead);
+            return false;
+        }
+        first = first->next;
+        second = second->next;
+    }
+    head = reverseLL(newHead);
+    return true;
+}
+
+Node *collison(Node *head1, Node *head2, int diff)
+{
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    // 1 : apply greater list pointer ( i.e temp2 ) to diff steps forward
+    while (diff)
+    {
+        diff--;
+        temp2 = temp2->next;
+    }
+    // 2 : now compare both LL for equality ,loop will break when both are equal either on a node or on null  ,  return any one of them
+    while (temp1 != temp2)
+    {
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return temp1;
+}
+
+Node *intersectingPoint(Node *head1, Node *head2)
+{
+    // brute T.C = o( {n * map ki T.C [logn or 1] } + {n2 * map ki T.C [logn or 1] } ) ||  S.C = o(n1) if u r hashing 1st LL
+    // map<Node *, int> mpp;
+    // Node *temp = head1;
+    // // 1: hashing the nodes from 1st LL
+    // while (temp)
+    // {
+    //     mpp[temp] = 1;
+    //     temp = temp->next;
+    // }
+    // // 2: checking the nodes from 2nd LL if they exist in our hashmap
+    // temp = head2;
+    // while (temp)
+    // {
+    //     if (mpp.find(temp) != mpp.end())
+    //     {
+    //         return temp;
+    //     }
+    //     temp = temp->next;
+    // }
+    // return NULL;
+
+    // optimal
+
+    // 1 : find length of 1st LL
+    Node *temp = head1;
+    int count1 = 0;
+    while (temp)
+    {
+        count1++;
+        temp = temp->next;
+    }
+
+    // 2 : find length of 2nd LL
+    temp = head2;
+    int count2 = 0;
+    while (temp)
+    {
+        count2++;
+        temp = temp->next;
+    }
+    // 3 : call the function with passing smaller list as first arguments which will do remaining steps
+    int diff = abs(count1 - count2);
+    if (count1 < count2)
+    {
+        return collison(head1, head2, count2 - count1);
+    }
+    else
+    {
+        return collison(head2, head1, count1 - count2);
+    }
+}
+
+Node *addOne(Node *head)
+{
+    head = reverseLL(head);
+    Node *temp = head;
+    int carry = 1;
+
+    while (temp != NULL)
+    {
+        temp->data = temp->data + carry;
+        if (temp->data < 10)
+        {
+            carry = 0;
+            break;
+        }
+        else
+        {
+            temp->data = 0;
+            carry = 1;
+        }
+        temp = temp->next;
+    }
+
+    if (carry == 1)
+    {
+        Node *newNode = new Node(1);
+        head = reverseLL(head);
+        newNode->next = head;
+        return newNode;
+    }
+    head = reverseLL(head);
+    return head;
+}
+
+Node *addTwo(Node *head1, Node *head2)
+{
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    Node *dummyNode = new Node(-1);
+    Node *curr = dummyNode;
+    int carry = 0;
+    // int sum = 0;
+    while (temp1 != NULL || temp2 != NULL)
+    {
+        int sum = carry;
+        if (temp1)
+            sum = sum + temp1->data;
+        if (temp2)
+            sum = sum + temp2->data;
+
+        Node *newNode = new Node(sum % 10);
+        carry = sum / 10;
+
+        curr->next = newNode;
+        curr = curr->next;
+        if (temp1)
+            temp1 = temp1->next;
+        if (temp2)
+            temp2 = temp2->next;
+    }
+
+    if (carry)
+    {
+        Node *newNode = new Node(carry);
+        curr->next = newNode;
+    }
+    return dummyNode->next;
+}
+
 int main()
 
 {
     vector<int> arr;
     arr.push_back(1);
-    arr.push_back(2);
-    arr.push_back(3);
-    arr.push_back(4);
     arr.push_back(5);
-    // arr.push_back(6);
-    // arr.push_back(13);
-    // arr.push_back(65);
+    arr.push_back(1);
+    // arr.push_back(14);
+    // arr.push_back(1);
+    // arr.push_back(2);
+    // arr.push_back(1);
+    // arr.push_back(0);
+    vector<int> brr;
+    brr.push_back(1);
+    brr.push_back(2);
+    brr.push_back(0);
+    // arr.push_back(0);
 
     // ================================================================
     // find the middle node  of linked list
@@ -357,6 +743,49 @@ int main()
     // Node *head = convertArr2LL(arr);
     // int n = 2;
     // Node *ans = removeNthNodeBack(head, n);
+    // print(ans);
+
+    // ================================================================
+    // remove the middle node of a singly linked list
+    // Node *head = convertArr2LL(arr);
+    // Node *ans = removeMiddle(head);
+    // print(ans);
+
+    // ================================================================
+    // check  singly linked list is palindrome or not
+    // Node *head = convertArr2LL(arr);
+    // bool ans = isPalindrome(head);
+    // ans == true ? cout << "yes : " : cout << "no : ";
+
+    // ================================================================
+    // sort a singly linked list
+    // Node *head = convertArr2LL(arr);
+    // Node *ans = sortLL(head);
+    // print(ans);
+
+    // ================================================================
+    // sort ll with 0s 1s 2s   singly linked list
+    // Node *head = convertArr2LL(arr);
+    // Node *ans = sortZeroOneTwo(head);
+    // print(ans);
+
+    // ================================================================
+    // find the intersecting point of a  Y linked list
+    // Node *head = convertArr2LL(arr);
+    // Node *ans = intersectingPoint(head);
+    // print(ans);
+
+    // ================================================================
+    // add 1 to a number represented by given linked list
+    // Node *head = convertArr2LL(arr);
+    // Node *ans = addOne(head);
+    // print(ans);
+
+    // ================================================================
+    // add 2  numbers in   given linked list
+    // Node *head = convertArr2LL(arr);
+    // Node *head2 = convertArr2LL(brr);
+    // Node *ans = addTwo(head, head2);
     // print(ans);
 
     return 0;
